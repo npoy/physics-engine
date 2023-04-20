3237d7ed1b4f902bc19492b8f76e774ab2b0cbe7
Nicolas Poy <nicolas.poy@corp.smtsoftware.com>
2023-04-19 23:00:17 -0300

    Added Newtons 2nd law

--- Assets/Scripts/PhysicsEngine.cs
+++ Assets/Scripts/PhysicsEngine.cs
@@ -4,24 +4,17 @@ using UnityEngine;
 
 public class PhysicsEngine : MonoBehaviour
 {
+    public float mass = 1f;
     public Vector3 velocityVector; // average velocity this FixedUpdate()
     public Vector3 netForceVector;
     public List<Vector3> forceVectorList = new List<Vector3>();
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
     void FixedUpdate() {
         AddForces();
-        
-        if (netForceVector == Vector3.zero) {
-            transform.position += velocityVector * Time.deltaTime;
-        } else {
-            Debug.LogError("Unbalanced force detected, help!");
-        }
+        UpdateVelocity();
+
+        // Update position
+        transform.position += velocityVector * Time.deltaTime;
     }
 
     void AddForces() {
@@ -29,5 +22,10 @@ public class PhysicsEngine : MonoBehaviour
         foreach (Vector3 forceVector in forceVectorList) {
             netForceVector = netForceVector + forceVector;
         }
+    }
+
+    void UpdateVelocity() {
+        Vector3 accelerationVector = netForceVector / mass; // F=m*a => a=F/m
+        velocityVector += accelerationVector * Time.deltaTime; // a=(Vi-Vf)/t => v=a*t
     }
 }
