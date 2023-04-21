4bf5c60d66d3df04d630bb298f493f9a7599cf2f
Nicolas Poy <nicolas.poy@corp.smtsoftware.com>
2023-04-21 10:44:37 -0300

    Refactor

--- Assets/Scripts/PhysicsEngine.cs
+++ Assets/Scripts/PhysicsEngine.cs
@@ -7,25 +7,63 @@ public class PhysicsEngine : MonoBehaviour
     public float mass = 1f;
     public Vector3 velocityVector; // average velocity this FixedUpdate()
     public Vector3 netForceVector;
-    public List<Vector3> forceVectorList = new List<Vector3>();
+    private List<Vector3> forceVectorList = new List<Vector3>();
 
-    void FixedUpdate() {
-        AddForces();
-        UpdateVelocity();
-
-        // Update position
-        transform.position += velocityVector * Time.deltaTime;
+    // Use this for initialization
+    void Start() {
+        SetupThrustTrails();
     }
 
-    void AddForces() {
+    void FixedUpdate() {
+        RenderTrails();
+        UpdatePosition();
+    }
+
+    public void AddForce(Vector3 forceVector) {
+        forceVectorList.Add(forceVector);
+    }
+
+    void UpdatePosition() {
+        // Sum the forces and clear the list
         netForceVector = Vector3.zero;
         foreach (Vector3 forceVector in forceVectorList) {
             netForceVector = netForceVector + forceVector;
         }
-    }
+        forceVectorList.Clear();
 
-    void UpdateVelocity() {
+        // Calculate position change due to net force
         Vector3 accelerationVector = netForceVector / mass; // F=m*a => a=F/m
         velocityVector += accelerationVector * Time.deltaTime; // a=(Vi-Vf)/t => v=a*t
+        transform.position += velocityVector * Time.deltaTime;
     }
+
+    // Draw Forces
+    public bool showTrails = true;
+	private LineRenderer lineRenderer;
+	private int numberOfForces;
+	
+	void SetupThrustTrails () {
+		lineRenderer = gameObject.AddComponent<LineRenderer>();
+		lineRenderer.material = new Material(Shader.Find("Sprites/Default"));
+		lineRenderer.SetColors(Color.yellow, Color.yellow);
+		lineRenderer.SetWidth(0.2F, 0.2F);
+		lineRenderer.useWorldSpace = false;
+	}
+	
+	// Update is called once per frame
+	void RenderTrails () {
+		if (showTrails) {
+			lineRenderer.enabled = true;
+			numberOfForces = forceVectorList.Count;
+			lineRenderer.SetVertexCount(numberOfForces * 2);
+			int i = 0;
+			foreach (Vector3 forceVector in forceVectorList) {
+				lineRenderer.SetPosition(i, Vector3.zero);
+				lineRenderer.SetPosition(i+1, -forceVector);
+				i = i + 2;
+			}
+		} else {
+			lineRenderer.enabled = false;
+		}
+	}
 }
